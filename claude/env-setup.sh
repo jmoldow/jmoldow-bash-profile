60b90d6e6b67b3c2ed9711490f1c936cb5428f21
Jordan Moldow <jordan.moldow@equilibriumenergy.com>
2026-05-26 09:38:16 -0700

    Auto-activate Python virtualenv

--- claude/env-setup.sh
+++ claude/env-setup.sh
@@ -1,3 +1,16 @@
 #!/usr/bin/env bash
 export PAGER=cat
 export GIT_PAGER=cat
+
+ACTIVATE_PATHS="$(find . -mindepth 2 -maxdepth 3 -path '*/bin/activate' -type f)"
+
+if [[ -f bin/activate ]]; then
+  source bin/activate
+elif [[ $(wc -l <<<"$ACTIVATE_PATHS") -eq 1 ]]; then
+  source "$ACTIVATE_PATHS"
+else
+  ACTIVATE_PATHS="$(grep "env/bin/activate" <<<"$ACTIVATE_PATHS")"
+  if [[ $(wc -l <<<"$ACTIVATE_PATHS") -eq 1 ]]; then
+    source "$ACTIVATE_PATHS"
+  fi
+fi
